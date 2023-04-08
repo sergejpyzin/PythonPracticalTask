2b59151c8ae68097c978da3e011d0a2977e266e1
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-04-08 14:17:53 +0300

    Created Lessons1_Task8 files

--- Lessons1_Task2.py
+++ Lessons1_Task2.py
@@ -20,9 +20,9 @@ print(f"({firstNumber} + {secondNumber} + {thirdNumber})")
 
 # Решение задачи c использованием свойств строк
 
-# number = input("Введите трехзначное число:\n")
-#
-# sumDigitalNumbers = int(number[0]) + int(number[1]) + int(number[2])
-#
-# print(sumDigitalNumbers, end=" ")
-# print(f"({number[0]} + {number[1]} + {number[2]})")
+number = input("Введите трехзначное число:\n")
+
+sumDigitalNumbers = int(number[0]) + int(number[1]) + int(number[2])
+
+print(sumDigitalNumbers, end=" ")
+print(f"({number[0]} + {number[1]} + {number[2]})")
