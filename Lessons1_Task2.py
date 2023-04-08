ce5ade2f1671cce5b0264ffdf055a27ee49daab4
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-04-08 13:56:49 +0300

    Added List method in Lessons1_Task2 files

--- Lessons1_Task2.py
+++ Lessons1_Task2.py
@@ -5,6 +5,8 @@
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0) |
 
+# Решение задачи без использования свойств строк
+
 number = int(input("Введите трехзначное число:\n"))
 
 firstNumber = number // 100
@@ -12,4 +14,15 @@ secondNumber = (number // 10) % 10
 thirdNumber = number % 10
 
 print(firstNumber + secondNumber + thirdNumber, end=" ")
-print(f"({firstNumber} + {secondNumber} + {thirdNumber})", sep="+")
+print(f"({firstNumber} + {secondNumber} + {thirdNumber})")
+
+
+
+# Решение задачи c использованием свойств строк
+
+# number = input("Введите трехзначное число:\n")
+#
+# sumDigitalNumbers = int(number[0]) + int(number[1]) + int(number[2])
+#
+# print(sumDigitalNumbers, end=" ")
+# print(f"({number[0]} + {number[1]} + {number[2]})")
