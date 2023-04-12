5b561f64e654978e82190d49110625d9cd6819ef
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-04-12 16:46:14 +0300

    Changed Lessons2_Task12 files. Added discriminant method

--- Lessons2_Task12.py
+++ Lessons2_Task12.py
@@ -16,3 +16,14 @@ for i in range (summa):
             break
 
 print(first_number, second_number)
+
+# Решение через дискриминант
+
+# summa = int(input("Сумма двух чисел равна:\n"))
+# multy = int(input("Произведение двух чисел равна:\n"))
+
+discrim = summa ** 2 - multy * 4
+number_a = int((summa - discrim ** 0.5) / 2)
+number_b = int((summa + discrim ** 0.5) / 2)
+
+print(number_a, number_b)
